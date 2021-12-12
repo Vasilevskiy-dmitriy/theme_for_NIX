b29d8e696d1029452a1e64ddc1383db9c0fa1613
Dmitriy <dima.vasilenko.com@gmail.com>
2021-12-12 21:58:16 +0200

    update-Issue#3:adaptive-menu

--- header.php
+++ header.php
@@ -48,6 +48,11 @@
 
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
         <div class="container-fluid">
+            <a class="navbar-brand" href="">Меню</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
             <div class="collapse navbar-collapse" id="navbarsExample03">
                 <ul class="navbar-nav me-auto mb-2 mb-sm-0">
                     <li class="nav-item">
@@ -74,6 +79,7 @@
             </div>
         </div>
     </nav>
+
 
 
 <!--    <div class="nav-scroller py-1 mb-2">-->
