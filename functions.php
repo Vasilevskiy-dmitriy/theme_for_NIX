820fd6d7f5a0bcfe5425f7158f87c3549bbe1c47
Dmitriy <dima.vasilenko.com@gmail.com>
2022-01-01 02:56:40 +0200

    Lesson#6|Part#2: Likes/Dislikes

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ include 'classes/modules/RegisterTaxonomy.php';
 include 'classes/modules/RegisterCPT.php';
 include 'classes/modules/CustomFilterCPT.php';
 include 'classes/modules/RegisterACF.php';
+include 'classes/modules/Likes.php';
 
 /**
  * NEW classes
@@ -21,7 +22,5 @@ new \theme_for_nix\classes\modules\RegisterTaxonomy();
 new \theme_for_Nix\classes\modules\RegisterCPT();
 new \theme_for_Nix\classes\modules\CustomFilterCPT();
 new \theme_for_Nix\classes\modules\RegisterACF();
-
-
-
+new \theme_for_Nix\classes\modules\Likes();
 
